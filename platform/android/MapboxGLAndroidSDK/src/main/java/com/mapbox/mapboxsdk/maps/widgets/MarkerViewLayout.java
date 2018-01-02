6794d9d53ac9dea96a912c63d70ff120d806646c
Tien TRAN-THUONG <tientranthuong@MacBook-Pro-de-Tien.local>
2018-01-02 16:44:03 +0100

    [ANMA-6084] small improving

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/widgets/MarkerViewLayout.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/widgets/MarkerViewLayout.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.logging.Logger;
 
+@Deprecated
 public class MarkerViewLayout extends FrameLayout {
 
     private MarkerViewManager markerViewManager;
