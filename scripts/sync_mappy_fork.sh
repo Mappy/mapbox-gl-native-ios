89ac74cded4836cbe2aacd9aaf168d3652052dcb
Romain Quidet <contact@xdappfactory.com>
2019-12-10 16:54:27 +0100

    [Mappy] enhance sync script

--- scripts/sync_mappy_fork.sh
+++ scripts/sync_mappy_fork.sh
@@ -2,12 +2,25 @@
 
 # sync_mappy_fork - A script to Mappy fork of mbgl with Mapbox repository
 
+echo "Checking remote upstream"
 git remote -v | grep 'upstream' &> /dev/null || git remote add upstream https://github.com/mapbox/mapbox-gl-native-ios.git
 git remote update
-#git fetch upstream master
-git checkout master
+
+echo "Fetching upstream master"
+git fetch upstream master
+
+echo "Updating local master"
+git checkout master --recurse-submodules
 git pull origin master --rebase
+
+echo "Merging remote master with local one"
 git merge upstream/master
+
+echo "Pushing updates"
+if [[ $1 == "debug" ]]; then
+    echo "Debug, no push done"
+    exit
+fi
 git push origin master
 git push --tags
 git checkout -
